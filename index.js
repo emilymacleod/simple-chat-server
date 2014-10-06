fb01a7c8372725425d551b92074bcb56270aae34
emilymacleod <margaretemacleod@gmail.com>
2014-10-06 17:04:50 -0300

    changed to res.sendFile(__dirname + '/index.html')

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-var app = require("express")();
-var http = require("http").Server(app);
-
-<<<<<<< HEAD
-app.get('/', function(req, res){
-  res.sendFile('index.html');
-});
-http.listen(3000, function(){
-	console.log("listening on *:3000");
-});
-=======
-app.get("/", function(req, res){
-	res.send("<h1>Hello world</h1>");
-});
-http.listen(3000, function(){
-	console.log("listening on *:3000");
-});
+var app = require('express')();
+var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.sendFile('index.html');
+  res.sendFile(__dirname + '/index.html');
 });
->>>>>>> FETCH_HEAD
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
