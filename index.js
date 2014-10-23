19dc32b771d626b089de89ca4ccfd1de787b13f6
emilymacleod <margaretemacleod@gmail.com>
2014-10-23 13:21:20 -0300

    added embedded feature
    added embedded feature

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var redis = require("redis");
+  client = redis.createClient();
+var userCount=0;
+var usernames = {};
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+app.get('/home', function(req, res){
+  res.sendFile(__dirname + '/views/home.html')
+});
+
+app.get('/r', function(req, res){
+  res.sendFile(__dirname + '/views/room.html')
+});
+
+app.get('/create', function(req, res){
+  res.sendFile(__dirname + '/views/createRoom.html')
 });
 
 app.use("/style.css", express.static(__dirname + '/style.css'));
@@ -16,27 +32,43 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
+
+client.set("app name", "simple chat", redis.print);
+
 io.on('connection', function(socket){
-  console.log('a user connected');
+  userCount= userCount+1;
+ console.log('a user connected','there are now',userCount,"users online");
+
+
+ client.get('app name', function(err, reply) {
+    if(err) {console.log(err)};
+    console.log('app name is', reply);
+  });
+  //redis gets array named history
+  client.hgetall('history', function(err, replies) {
+    if(err){console.log(err) }; 
+    socket.emit('gotHistory', replies);
+  });
+
+  socket.on('sentMessage', function(msg){
+      console.log("message received",msg);
+      io.emit('gotMessage', msg);
+  });
+
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    userCount = userCount-1;
+    console.log('a user disconnected','there are now',userCount,"users online");
   });
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-});
-
-io.emit('some event', { for: 'everyone' });
-
-io.on('connection', function(socket){
-  socket.broadcast.emit('hi');
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+ /*socket.on('talk', function(msg){
+    console.log('talk: ' + msg);
+    socket.broadcast.emit('talk', msg);
+    client.incr('msg_id', function(err, msg_id) {
+      console.log('msg_id', msg_id);
+      client.hset('history', msg_id, msg);
+    });
+  });*/
--- script.js
+++ script.js
@@ -9,13 +9,53 @@ var person = "";
   };
 };
 
+/*function addMessage() {
+  var newMsg = document.getElementById('newMsg');
+  var TextArea = document.getElementById('TextArea');
+  var msg = newMsg.value;
+  var content = document.createElement('div');
+  content.innerText = msg;
+  msgs.appendChild(content);
+}*/
+
+function addEmbed() {
+var embedInput = document.getElementById('embedInput');
+var iframe = document.createElement('iframe');
+/*
+var iframeUnique  = {};
+iframeUnique.height=iframe.height;
+iframeUnique.width=iframe.width;
+iframeUnique.src=iframe.src;
+*/
+var embedded = embedInput.value;
+console.log("embedded for server is",embedded);
+console.log("room title for server is","non-existent")
+
+
+  document.getElementById('activeEmbed').innerHTML=embedded;
+}
+
+
 var socket = io();
+
+
+socket.on('gotHistory', function(replies){
+  console.log('history is',replies)
+  for (var i in replies) {
+      var msg = replies[i];
+      $('#TextArea').innerHTML=$('<div>').text(msg);
+    };
+})
+
   $('form').submit(function(){
-    socket.emit('chat message', person + " " + "said: " + $('#m').val());
+
+    var msg = person + " " + "said: " + $('#m').val();
+    console.log("message to be sent is",msg)
+    socket.emit('sentMessage',msg);
     $('#m').val('');
     return false;
   });
 
-  socket.on('chat message', function(msg){
+  socket.on('gotMessage', function(msg){
     $('#TextArea').append($('<p>').text(msg));
   });
